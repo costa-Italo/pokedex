218b7c3b2f8433302e82014e1e62da8594821a4f
Italo Costa <italosiqueiradacosta@gmail.com>
2024-04-17 09:49:40 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./globals/Globals"
+
+function App() {
+
+  return (
+
+    <div>
+        <GlobalStyle/>
+      <h1>Pokedex - Primeiro Commit</h1>
+    
+    </div>
+  )
+}
+
+export default App
