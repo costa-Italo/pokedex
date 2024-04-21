e368859c4b028d46fd5a496ea3df2fe42713620a
Italo Costa <italosiqueiradacosta@gmail.com>
2024-04-21 11:02:18 -0300

    🔹 Links para github e linkedin adicionados.

--- src/components/Headline/index.tsx
+++ src/components/Headline/index.tsx
@@ -8,14 +8,27 @@ const Headline = () => {
             <SubHeadlineStyles>Descubra e explore o mundo dos Pokémon como nunca antes com a Pokédex: a sua companheira essencial para mergulhar na vastidão dos Pokémons. Pesquise, aprenda e mergulhe na riqueza da história e das características de cada criatura, enquanto viaja pelos diferentes universos. Desde os clássicos aos mais recentes, a Pokédex oferece uma experiência imersiva e informativa, permitindo que você se torne um verdadeiro mestre!</SubHeadlineStyles>
             
             <IconsStyles>
-                <GithubLogo 
-                    size={25}
-                    fill="black"
-                />
-                <LinkedinLogo 
-                    size={25}
-                    fill="black"
-                />      
+                <a 
+                    href="https://github.com/costa-Italo/pokedex"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >    
+                        <GithubLogo 
+                            size={25}
+                            fill="black"
+                        />
+                        </a>
+
+                    <a 
+                        href="https://www.linkedin.com/in/italo-costa-dev/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >        
+                        <LinkedinLogo 
+                            size={25}
+                            fill="black"
+                        />   
+                    </a>   
             </IconsStyles>
         </div>
     )
