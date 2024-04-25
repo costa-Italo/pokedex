a7307bd0e321826a5b91974aad772eb030f096bd
Italo Costa <italosiqueiradacosta@gmail.com>
2024-04-25 10:06:59 -0300

    💥 Retornando pokemons na tela.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import CardPokemon from "./components/Card"
 import { CardsConstainer } from "./components/Card/styles"
 import Header from "./components/Header"
@@ -7,7 +7,28 @@ import { GlobalStyle } from "./globals/Globals"
 
 function App() {
 
-  const [pokemons, SetPokemons] =useState([1]);
+  const [pokemons, SetPokemons] =useState([0]);
+
+  const getApiData = async () => {
+    const endpoints = [];
+
+    try {
+      for(let i = 1; i <= 50; i++) {
+        endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+      }
+      await Promise.all(endpoints.map((endpoint => fetch(endpoint))))
+      .then ((res) => Promise.all(res.map(async r => r.json())))
+      .then (res =>
+        SetPokemons(res)
+      )
+    } catch (error) {
+      console.log(error)
+    } 
+  }
+
+  useEffect(() => {
+    getApiData();
+  },[])  
 
   return (
 
