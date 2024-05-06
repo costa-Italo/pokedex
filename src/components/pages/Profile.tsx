2dab85b0336f5d1025d9c442d1d2343b8ddb7c21
Italo Costa <italosiqueiradacosta@gmail.com>
2024-05-06 15:23:04 -0300

    🔸 Retornando tipo e peso do pokemon de cada pokemon específico.

--- src/components/pages/Profile.tsx
+++ src/components/pages/Profile.tsx
@@ -7,23 +7,34 @@ const Profile = () => {
     const { pokemonName } = useParams<{ pokemonName: string }>(); 
     const location = useLocation();
     const [abilities, setAbilities] = useState<string[]>([]);
+    const [types, setTypes] = useState<string[]>([]);
+    const [weight, setWeight] = useState<number | null>(null);
 
     useEffect(() => {
-        const fetchPokemonAbilities = async () => {
+        const fetchData = async () => {
             try {
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
                 if (!response.ok) {
                     throw new Error("Failed to fetch data");
                 }
                 const data = await response.json();
-                const pokemonAbilities = data.abilities.map((ability:{ ability: {name:string } }) => ability.ability.name);
+                
+                // Extrair habilidades
+                const pokemonAbilities = data.abilities.map((ability: { ability: { name: string } }) => ability.ability.name);
                 setAbilities(pokemonAbilities);
+                
+                // Extrair tipos
+                const pokemonTypes = data.types.map((type: { type: { name: string } }) => type.type.name);
+                setTypes(pokemonTypes);
+                
+                // Extrair peso
+                setWeight(data.weight);
             } catch (error) {
-                console.error("Error fetching Pokemon abilities:", error);
+                console.error("Error fetching Pokemon data:", error);
             }
         };
 
-        fetchPokemonAbilities();
+        fetchData();
     }, [pokemonName]);
 
     return (          
@@ -35,17 +46,27 @@ const Profile = () => {
                 )}
             </ProfileHeader>
             <PokemonAbilitiesContainer>
-                <h2>Abilities:</h2>
+                <h2>Abilidades</h2>
                 <ul>
                     {abilities.map((ability, index) => (
                         <li key={index}>{ability}</li>
                     ))}
+
+<h2>Tipos</h2>
+                <ul>
+                    {types.map((type, index) => (
+                        <li key={index}>{type}</li>
+                    ))}
+
+<h2>Peso</h2>
+                <p>{weight !== null ? `${weight} kg` : 'Carregando...'}</p>
+                </ul>
                 </ul>
             </PokemonAbilitiesContainer>
+        
             <ProfileNameContainer>
                 <PokemonNameProfile>{pokemonName}</PokemonNameProfile>
             </ProfileNameContainer>
-
         </ProfileContainer>
     );
 };
